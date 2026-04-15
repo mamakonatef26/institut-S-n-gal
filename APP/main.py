8f3c89a9371f73450523d0d81e8e6a47ce9759bc
mamakonatef26 <mamakonatef@gmail.com>
2026-04-15 12:46:29 +0000

    Add files via upload

---
+++ APP/main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.set_page_config(layout="wide")
+st.title("Cartographie des Instituts et Réseaux au Sénégal")
+
+with open("APP/index.html", 'r', encoding='utf-8') as f:
+    html_data = f.read()
+    components.html(html_data, height=700)
