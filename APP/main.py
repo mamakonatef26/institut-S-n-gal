50aff8367fa919e21f225c6d4d8281a7164ee4db
mamakonatef26 <mamakonatef@gmail.com>
2026-04-15 12:56:14 +0000

    Update main.py

--- APP/main.py
+++ APP/main.py
@@ -4,6 +4,6 @@ import streamlit.components.v1 as components
 st.set_page_config(layout="wide")
 st.title("Cartographie des Instituts et Réseaux au Sénégal")
 
-with open("APP/index.html", 'r', encoding='utf-8') as f:
+with open("index.html", 'r', encoding='utf-8') as f:
     html_data = f.read()
-    components.html(html_data, height=700)
+    components.html(html_data, height=700, scrolling=True)
