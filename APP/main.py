5cb38f7c72b8f66a6cf75e5c0aabb580b03179ba
mamakonatef26 <mamakonatef@gmail.com>
2026-04-15 12:59:34 +0000

    Update main.py

--- APP/main.py
+++ APP/main.py
@@ -1,9 +1,16 @@
 import streamlit as st
 import streamlit.components.v1 as components
+import os
 
 st.set_page_config(layout="wide")
 st.title("Cartographie des Instituts et Réseaux au Sénégal")
 
-with open("index.html", 'r', encoding='utf-8') as f:
-    html_data = f.read()
-    components.html(html_data, height=700, scrolling=True)
+current_dir = os.path.dirname(__file__)
+file_path = os.path.join(current_dir, "index.html")
+
+try:
+    with open(file_path, 'r', encoding='utf-8') as f:
+        html_data = f.read()
+        components.html(html_data, height=700, scrolling=True)
+except FileNotFoundError:
+    st.error("Le fichier index.html est introuvable dans le dossier APP.")
